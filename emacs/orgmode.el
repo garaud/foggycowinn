81293b923a5db2f8aa49bc82e8cfc6dff2cd726a
Damien Garaud <damien.garaud@gmail.com>
2011-05-26 20:00:40 +0200

    Imported 'orgmode.el' for Emacs configuration.

---
+++ emacs/orgmode.el
@@ -0,0 +1,34 @@
+;; -*- coding: utf-8 -*-
+;; Emacs configuration file for org-mode
+;; 2011
+;; Author(s): Damien Garaud
+
+;; Remember.
+;;(org-remember-insinuate)
+(setq org-remember-templates
+      '(("Agenda" ?a "* %?" "~/org/agenda.org" bottom)))
+;;         ;; ("Note" ?n "%?" "~/org/note.org" bottom)
+;;         ;; ("Répondre" ?r "* TODO Répondre à %a :répondre:\n  %?"
+;;         ;;  "~/org/todo.org" bottom)
+;;         ;; ("todo" ?t "* TODO %? %^g" "~/org/todo.org" bottom)))
+;; Location of the agenda, for org-mode.
+(setq org-agenda-files (quote ("~/org/agenda.org"
+                               "~/org/todo/work.org"
+                               "~/org/todo/home.org")))
+
+;; Diary.
+;;(setq org-agenda-include-diary t)
+;; Highlight the current line in agenda views.
+;;(add-hook 'org-agenda-mode-hook 'highlight-current-line-minor-mode)
+
+
+;;;;;;;;;;;;;;;;;
+;; Keybindings ;;
+;;;;;;;;;;;;;;;;;
+
+;; Agenda.
+(define-key global-map "\C-ca" 'org-agenda)
+;; Remember.
+(define-key global-map "\C-cr" 'org-remember)
+
+(define-key global-map "\C-cL" 'org-store-link)
