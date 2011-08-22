4fd685178fe83628f30fdc3a17899d9ec06126c7
Damien Garaud <damien.garaud@logilab.fr>
2011-08-22 11:41:54 +0200

    Added functions dedicated to Mercurial repo.

--- emacs/foggy_git.el
+++ emacs/foggy_git.el
@@ -1,8 +1,50 @@
 ;; -*- coding: utf-8 -*-
-;; Main Emacs configuration file.
+;; Git and Hg Emacs configuration file.
 ;; 2011
 ;; Author(s): Damien Garaud
+
+;;;;;;;;;
+;; GIT ;;
+;;;;;;;;;
 
 ;; Magit.
 (require 'magit)
 (global-set-key "\C-cg" 'magit-status)
+
+
+;;;;;;;;;;;;;;;
+;; Mercurial ;;
+;;;;;;;;;;;;;;;
+
+;;; This version has better chances to run on windows
+(setq hg-grep-command "grep --color=always -nH -e ")
+(setq hg-grep-tempfile (make-temp-file "grep"))
+
+(defun hg-grep (s)
+  (interactive "sSearch project for: ")
+
+  (setq hgroot (car (split-string (shell-command-to-string "hg root") "\n")))
+  ;; replace hgroot absolute path with relative path
+  ;; this should reduce the line size in the grep buffer
+  (setq hgroot (file-relative-name hgroot))
+  (setq hgmanifest (split-string (shell-command-to-string "hg manifest")))
+  
+  (setq cmd (concat hg-grep-command "\"" s "\" `cat " hg-grep-tempfile "`"))
+  (setq files "")
+
+  (while (>= (length hgmanifest) 1)
+    ;;(setq cmd (concat cmd " " hgroot "/" (car hgmanifest)))
+    (setq files (concat files hgroot "/" (car hgmanifest) "\n"))
+    (setq hgmanifest (cdr hgmanifest))
+    )
+  (write-region files nil hg-grep-tempfile)
+
+  ;;(setq c md (concat cmd "| sed -e \"s%" hgroot "%HG%\""))
+  (compilation-start cmd 'grep-mode)
+)
+
+(defun hg-grep-region (start end)
+  (interactive "r")
+
+  (hg-grep (buffer-substring start end))
+)
