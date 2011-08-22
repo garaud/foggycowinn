4ac0aaf9812bd4038fa50248f8fdac708eff2e7f
Damien Garaud <damien.garaud@logilab.fr>
2011-08-22 11:47:23 +0200

    ---

--- emacs/foggy_appearance.el
+++ emacs/foggy_appearance.el
@@ -113,7 +113,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(ecb-options-version "2.40")
  '(frame-background-mode (quote dark)))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
