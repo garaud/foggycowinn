ad861062d57f48ea9bba2d22a6bd55d3625eb7e6
Damien Garaud <damien.garaud@gmail.fr>
2012-12-20 23:51:03 +0100

    [emacs] Update the bash script which downloads some Emacs packages
    - Turn the Shell script into a Bash script
    - Can take into account no-Git URL with a dummy test.

--- emacs/package_download.sh
+++ emacs/package_download.sh
@@ -1,45 +1,49 @@
-#!/bin/sh
-# Damien Garaud
-# A basic Shell script to copy/clone Emacs packages into a directory. The name
-# and Git URL of each Emacs package are specified in the text file
-# 'package_list'.
+#!/bin/bash
+# Author: Damien Garaud
+# A basic Bash script to copy/clone Emacs packages into a directory. The name
+# and URL of each Emacs package are specified in the text file 'package_list'.
 
 # Path where you would like to copy/clone some Emacs packages.
 # It can be '$HOME/.emacs.d/' for instance.
 package_dir=$HOME/applications/emacs
 
+if [ ! -d $package_dir ]; then
+    echo "'$package_dir' does not exist."
+    echo "Please set the 'package_dir' variable in this file."
+    exit 0
+else
+    package_dir=$HOME/.emacs.d
+fi
+
+# Go to the dir where different emacs packages will be.
+pushd $package_dir
+
 # XXX This path must be the same as the path specified in your '.emacs' with the
-# variable 'emacs-foggy-package-dir'. It you don't have this variable, please add it.
+# variable 'emacs-foggy-package-dir'. It you don't have this variable, please
+# add it.
 # (setq emacs-foggy-package-dir "~/applications/emacs")
 
 
 # Package name specified in the text file 'package_list' (with Git URL).
 package_list_name=`awk '{print $1}' package_list`
 
-# Function which copy/clone a list of Emacs packages.
-function clone
-{
-    echo "Ckeck packages..."
-    for name in $package_list_name; do
-        # Check if the package has already cloned.
-        if [ -d $package_dir"/"$name ]; then
-            echo "'$name' already exist."
-        else
-            here=`pwd`
-            url=`grep $name package_list | awk '{print $2}'`
-            cd $package_dir
-            git clone $url
-            cd $here
-        fi
-    done
-}
+# Loop on package list.
+echo "Check packages..."
+for name in $package_list_name; do
+    # Check if the package has already cloned.
+    if [ -d $package_dir"/"$name ]; then
+        echo "'$name' already exist."
+    else
+	url=`grep $name package_list | awk '{print $2}'`
+	# XXX Handle the Mercurial case.
+	echo $url
+        if [[ "$url" == *git* ]]; then
+	    git clone $url
+	else
+	    wget $url
+	fi
+    fi
+done
 
-
-echo "Check '$package_dir'"
-if [ -d $package_dir ]; then
-    clone
-else
-    echo "Create '$package_dir'"
-    mkdir $package_dir
-    clone
-fi
+# Go back to the foggycowin dir.
+popd
