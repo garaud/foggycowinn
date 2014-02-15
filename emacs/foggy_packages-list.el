70e8ebd72676d857507d2c83f7713051a609c685
Damien Garaud <damien.garaud@gmail.com>
2014-02-15 21:58:04 +0100

    [emacs] Add file with the packages to installed/used.

---
+++ emacs/foggy_packages-list.el
@@ -0,0 +1,22 @@
+;; -*- coding: utf-8 -*-
+;; Emacs configuration file for the list of used packages.
+;; 2014
+;; Author(s): Damien Garaud
+
+(defvar foggy-packages-list
+  '(
+    ag
+    auctex
+    auto-complete fuzzy
+    cmake-mode
+    ido-ubiquitous
+    lua-mode
+    jinja2-mode
+    magit
+    monky
+    ;;mingus
+    org
+    yaml-mode
+    yasnippet
+    )
+  "List of packages.")
