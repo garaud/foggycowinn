6306429d1c5b4a0cddb3337a18a007ce2fb407e2
Damien Garaud <damien.garaud@gmail.com>
2014-02-15 22:12:54 +0100

    [emacs] Update the Web configuration file.
    * Only keep the 'erc' package.
    * Remove the oauth and identica packages.

--- emacs/foggy_web.el
+++ emacs/foggy_web.el
@@ -1,16 +1,7 @@
 ;; -*- coding: utf-8 -*-
 ;; Emacs configuration file for Web
-;; 2011
+;; 2011-2014
 ;; Author(s): Damien Garaud
-
-;; Loading path.
-;; (add-to-list 'load-path "~/applications/emacs/identica-mode/")
-;; (add-to-list 'load-path "~/applications/emacs/emacs-oauth/")
-(setq identica-path (format "%s/%s" emacs-foggy-package-dir "identica-mode"))
-(add-to-list 'load-path identica-path)
-(setq oauth-path (format "%s/%s" emacs-foggy-package-dir "emacs-oauth"))
-(add-to-list 'load-path oauth-path)
-
 
 ;; For IRC. Do 'M-x erc'.
 (require 'erc)
@@ -20,14 +11,6 @@
 ;; Save when quit.
 (setq erc-save-buffer-on-part t)
 
-;; Identica
-;; Download and see source code at http://git.savannah.gnu.org/cgit/identica-mode.git
-;; This mode support oauth. So, you also need emacs-oauth
-;; emacs-oauth: https://github.com/psanford/emacs-oauth/
 
-(require 'identica-mode)
-;; (setq identica-username "Username")
-;; (setq identica-password "Password")
-
-;; Post from the minibuffer without having an identica buffer active.
-(global-set-key "\C-cip" 'identica-update-status-interactive)
+(setq foggy_dev-loaded t)
+(provide 'foggy_web)
