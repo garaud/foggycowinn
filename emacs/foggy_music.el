cc193ea7a1b4ada49b07347ea0986ce64440ceaf
Damien Garaud <damien.garaud@gmail.com>
2014-02-15 22:10:19 +0100

    [emacs] Update the music configuration file.
    Disable the Mingus (MPD client) package for now (not available on ELPA or
    Marmelade mirrors).

--- emacs/foggy_music.el
+++ emacs/foggy_music.el
@@ -1,10 +1,13 @@
 ;; -*- coding: utf-8 -*-
 ;; Emacs configuration file for music
-;; 2011
+;; 2011-2014
 ;; Author(s): Damien Garaud
 
 ;; Loading path.
-(setq mingus-path (format "%s/%s" emacs-foggy-package-dir "mingus"))
-(add-to-list 'load-path mingus-path)
+;; (setq mingus-path (format "%s/%s" emacs-foggy-package-dir "mingus"))
+;; (add-to-list 'load-path mingus-path)
 
-(autoload 'mingus "mingus-stays-home" nil t)
+;; (autoload 'mingus "mingus-stays-home" nil t)
+
+(setq foggy_music-loaded t)
+(provide 'foggy_music)
