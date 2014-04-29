11d45b33be2d45a1d8205e15f4e8f59de4d90527
Damien Garaud <damien.garaud@gmail.com>
2014-04-30 00:17:44 +0200

    [emacs] dev: configure the 'multiple-cursors' package.

--- emacs/foggy_dev.el
+++ emacs/foggy_dev.el
@@ -10,6 +10,18 @@
 ;; Yasnippet.
 (yas-global-mode 1)
 
+;; Multiple cursors
+(require 'multiple-cursors)
+;; On an active region with multiple lines add a cursor to each line:
+;; NOTE: 'C-S-c' means 'C-MAJ-c'
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+;; Add multiple cursors based on a keyword in the buffer (first set-mark your
+;; keyword, then 'C->').
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+
 ;;;;;;;;;;
 ;; YAML ;;
 ;;;;;;;;;;
