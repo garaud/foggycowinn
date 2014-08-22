0eba385b92aa1b0f8a1d5c66a373dd0e29ebf73a
Damien Garaud <damien.garaud@gmail.com>
2014-08-22 15:14:14 +0200

    [ipython] datasci: add two new functions to generate a Panel and a MultiIndex DataFrame

--- ipython/datasci/startup/01-data-func.py
+++ ipython/datasci/startup/01-data-func.py
@@ -64,11 +64,30 @@ def _gen_dataframe_types():
                          'U': [3, 6, 2, 7, 2, 4, 3, 7, 2],
                          'T': ['AA', 'FG', 'FG', 'AA', 'FG', 'KL', 'KL', 'AA', 'MV']})
 
+def _gen_multiindex_dataframe():
+    """Generate a DataFrame with 2 cols and a MultiIndex.
+    """
+    idx_level = (['A', 'A', 'A', 'C', 'C', 'C'],
+                 [1, 2, 3, 1, 2, 3])
+    index = pd.MultiIndex.from_tuples(zip(*idx_level))
+    return pd.DataFrame(np.random.randn(6, 2), index=index, columns=['X','Y'])
+
+
+def _gen_panel():
+    """Generate a simple Panel with three keys.
+    """
+    df_index_size = 25
+    return pd.Panel({'cat_S': _gen_random_dataframe(df_index_size, freq='D'),
+                     'cat_T': _gen_random_dataframe(df_index_size, freq='B'),
+                     'cat_U': _gen_random_dataframe(df_index_size, freq='D')})
+
 a = _gen_random_array()
 s = _gen_str_series()
 ts = _gen_timeseries()
 df = _gen_random_dataframe()
 dfm = _gen_dataframe_types()
+dfmidx = _gen_multiindex_dataframe()
+panel = _gen_panel()
 print_datasci_version()
 msg = """\nVariables:
 - 'a': normal random array
@@ -76,5 +95,7 @@ msg = """\nVariables:
 - 'ts': Series with dates
 - 'df': DataFrame 3 columns of random float and int
 - 'dfm': DataFrame 3 columns multiple data: float, int, str
+- 'dfmidx': DataFrame with 2 cols and a MultiIndex
+- 'panel': Panel with three random DataFrame (3 columns)
 """
 print(msg)
