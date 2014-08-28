aaee6192601caa9b2bf2b6756b0271c9ca8ff702
Damien Garaud <damien.garaud@gmail.com>
2014-08-28 13:52:47 +0200

    [emacs] DVCS: not need anymore of (unused) hg-grep function
    I've 'ag.el' and projectile. I think it's enough.

--- emacs/foggy_revision_control.el
+++ emacs/foggy_revision_control.el
@@ -29,38 +29,5 @@
 ;;(setq monky-process-type 'cmdserver)
 
 
-;;; This version has better chances to run on windows
-(setq hg-grep-command "grep --color=always -nH -e ")
-(setq hg-grep-tempfile (make-temp-file "grep"))
-
-(defun hg-grep (s)
-  (interactive "sSearch project for: ")
-
-  (setq hgroot (car (split-string (shell-command-to-string "hg root") "\n")))
-  ;; replace hgroot absolute path with relative path
-  ;; this should reduce the line size in the grep buffer
-  (setq hgroot (file-relative-name hgroot))
-  (setq hgmanifest (split-string (shell-command-to-string "hg manifest")))
-  
-  (setq cmd (concat hg-grep-command "\"" s "\" `cat " hg-grep-tempfile "`"))
-  (setq files "")
-
-  (while (>= (length hgmanifest) 1)
-    ;;(setq cmd (concat cmd " " hgroot "/" (car hgmanifest)))
-    (setq files (concat files hgroot "/" (car hgmanifest) "\n"))
-    (setq hgmanifest (cdr hgmanifest))
-    )
-  (write-region files nil hg-grep-tempfile)
-
-  ;;(setq c md (concat cmd "| sed -e \"s%" hgroot "%HG%\""))
-  (compilation-start cmd 'grep-mode)
-)
-
-(defun hg-grep-region (start end)
-  (interactive "r")
-
-  (hg-grep (buffer-substring start end))
-)
-
 (setq foggy_revision_control-loaded t)
 (provide 'foggy_revision_control)
