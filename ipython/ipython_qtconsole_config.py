5a579f106f05c1439205981b3be411f80e1e3e93
Damien Garaud <damien.garaud@gmail.com>
2014-12-15 09:28:25 +0100

    ipython: add content to the Qt configuration file

--- ipython/ipython_qtconsole_config.py
+++ ipython/ipython_qtconsole_config.py
@@ -2,4 +2,13 @@
 
 c = get_config()
 
-# Quite empty, I never launch the Qt console.
+# Start the console window maximized.
+c.IPythonQtConsoleApp.maximize = True
+
+# Set to display confirmation dialog on exit. You can always use 'exit' or
+# 'quit', to force a direct exit without any confirmation.
+c.IPythonQtConsoleApp.confirm_exit = False
+
+# The font size. If unconfigured, Qt will be entrusted with the size of the
+# font.
+c.IPythonWidget.font_size = 11
