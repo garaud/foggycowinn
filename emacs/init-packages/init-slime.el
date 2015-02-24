907d944e6471314453f99a54dda39d6260e7330c
Damien Garaud <damien.garaud@gmail.com>
2015-02-24 16:11:42 +0100

    [emacs] slime config: add fancy-contrib

--- emacs/init-packages/init-slime.el
+++ emacs/init-packages/init-slime.el
@@ -1,3 +1,6 @@
+(require 'slime-autoloads)
+(add-to-list 'slime-contribs 'slime-fancy)
+
 (setq inferior-lisp-program "/usr/bin/sbcl")
 (eval-after-load "slime"
   '(progn
@@ -5,5 +8,3 @@
      (define-key slime-mode-map (kbd "M-n") nil)
      (define-key slime-mode-map (kbd "C-c d") 'slime-documentation)
      (define-key slime-mode-map (kbd "C-j") 'slime-eval-print-last-expression)))
-
-
