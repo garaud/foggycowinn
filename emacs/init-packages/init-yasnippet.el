a3c67f7ef9c044731068a7279e988ae476bd4426
Damien Garaud <damien.garaud@gmail.com>
2015-02-24 16:12:02 +0100

    [emacs] yas config: disable yas for Lisp and Slime REPL modes

--- emacs/init-packages/init-yasnippet.el
+++ emacs/init-packages/init-yasnippet.el
@@ -1,1 +1,7 @@
 (yas-global-mode 1)
+
+;; Disable yasnippet in Lisp and REPL Slime major modes.
+(add-hook 'lisp-mode-hook
+	  '(lambda () (yas-minor-mode 0)))
+(add-hook 'slime-repl-mode-hook
+	  '(lambda () (yas-minor-mode 0)))
