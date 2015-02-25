3c9488b0f354f1e77fb58f91febd33416d58d0a7
Damien Garaud <damien.garaud@gmail.com>
2015-02-25 16:01:51 +0100

    [emacs] custom: use theme colors for company completion colors

--- emacs/foggy_custom.el
+++ emacs/foggy_custom.el
@@ -3,6 +3,8 @@
 ;; 2014
 ;; Author(s): Damien Garaud
 
+(require 'color)
+
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,6 +16,23 @@
  '(sp-highlight-wrap-overlay nil)
  '(sp-highlight-wrap-tag-overlay nil))
 
+;; Globally inherits from my theme for company colors.
+;; Inspired from https://github.com/noprompt/matilde/blob/master/emacs.d/noprompt-theme.el
+(defun set-company-faces ()
+  (let ((bg (face-attribute 'default :background)))
+  (custom-set-faces
+   `(company-tooltip
+     ((t (:inherit default :background ,(color-lighten-name bg 5)))))
+   `(company-scrollbar-bg
+     ((t (:background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-fg
+     ((t (:background ,(color-lighten-name bg 2)))))
+   `(company-tooltip-selection
+     ((t :inherit font-lock-function-name-face)))
+   `(company-tooltip-common
+     ((t :inherit font-lock-constant-face)))
+   )))
+
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,17 +43,9 @@
  ;; Keep the color but change the default size (was 1.5 and 1.2 resp.).
  `(org-level-1 ((t (:bold t :foreground "dodger blue" :height 1.0))))
  `(org-level-2 ((t (:bold t :foreground "#edd400" :height 1.0))))
- ;; For Company
- `(company-tooltip ((t :background "lightgray" :foreground "black")))
- `(company-tooltip-selection ((t :background "steelblue" :foreground "white")))
- `(company-tooltip-mouse ((t :background "blue" :foreground "white")))
- `(company-tooltip-common ((t :background "lightgray" :foreground "black")))
- `(company-tooltip-common-selection ((t t :background "lightgray" :foreground "black")))
- `(company-scrollbar-fg ((t :background "black")))
- `(company-scrollbar-bg ((t :background "gray")))
- `(company-preview ((t :background nil :foreround "darkgray")))
- `(company-preview-common ((t :background nil :foreround "darkgray"))))
-)
+ ))
+
+(set-company-faces)
 
 (setq foggy-custom-loaded t)
 (provide 'foggy_custom)
