0fcd58ecbf3177b66b9609f64e0362f28b6b994f
Damien Garaud <damien.garaud@gmail.com>
2016-01-18 15:43:17 +0100

    emacs: update org-mode configuration

--- emacs/foggy_orgmode.el
+++ emacs/foggy_orgmode.el
@@ -1,20 +1,19 @@
 ;; -*- coding: utf-8 -*-
 ;; Emacs configuration file for org-mode
-;; 2011-2014
+;; 2011-2015
 ;; Author(s): Damien Garaud
 
-;; Remember.
-;;(org-remember-insinuate)
-(setq org-remember-templates
-      '(("Agenda" ?a "* %?" "~/org/agenda.org" bottom)))
-;;         ;; ("Note" ?n "%?" "~/org/note.org" bottom)
-;;         ;; ("Répondre" ?r "* TODO Répondre à %a :répondre:\n  %?"
-;;         ;;  "~/org/todo.org" bottom)
-;;         ;; ("todo" ?t "* TODO %? %^g" "~/org/todo.org" bottom)))
 ;; Location of the agenda, for org-mode.
-(setq org-agenda-files (quote ("~/org/agenda.org"
-                               "~/org/todo/work.org"
+(setq org-directory "~/org/todo")
+(setq org-agenda-files (quote ("~/org/todo/work.org"
                                "~/org/todo/home.org")))
+(setq org-capture-templates
+      '(("t" "Todo" entry
+         (file+headline "~/org/todo/tiny-tasks.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal Entry" plain
+         (file+datetree "~/org/journal.org")
+         "%U\n\n%?" :empty-lines-before 1)))
 
 ;; Diary.
 ;;(setq org-agenda-include-diary t)
@@ -37,7 +36,7 @@
 ;; Agenda.
 (define-key global-map "\C-ca" 'org-agenda)
 ;; Remember.
-(define-key global-map "\C-cr" 'org-remember)
+(define-key global-map "\C-cc" 'org-capture)
 
 (define-key global-map "\C-cL" 'org-store-link)
 
