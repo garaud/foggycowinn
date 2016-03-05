c40245c353eba2945db7d193d0c7efe43658eea5
Damien Garaud <damien.garaud@gmail.com>
2016-03-05 23:17:51 +0100

    [emacs] fix anaconda mode configuration

--- emacs/init-packages/init-anaconda-mode.el
+++ emacs/init-packages/init-anaconda-mode.el
@@ -1,5 +1,2 @@
 (add-hook 'python-mode-hook 'anaconda-mode)
-;; (add-hook 'python-mode-hook 'eldoc-mode)
-
-(global-unset-key [(meta r)])
-(global-set-key [(meta r)] "\C-a\C-@\C-e\C-f\C-w")
+(add-hook 'python-mode-hook 'anaconda-eldoc-mode)
