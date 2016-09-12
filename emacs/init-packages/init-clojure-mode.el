7eec83914db0be092ee7d1d2dc867fa260661248
Damien Garaud <damien.garaud@gmail.com>
2016-09-13 00:13:33 +0200

    [emacs] set some specific clojure indentations for compojure

--- emacs/init-packages/init-clojure-mode.el
+++ emacs/init-packages/init-clojure-mode.el
@@ -1,1 +1,12 @@
 (require 'clojure-mode)
+
+;; better indentation compjure's macros
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
