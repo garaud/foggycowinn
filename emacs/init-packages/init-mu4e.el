eda353f9b21a65f3a6ac409e31ff60638d88cc83
Damien Garaud <damien.garaud@gmail.com>
2017-03-19 09:57:04 +0100

    [emacs] org-contact with mu4e

--- emacs/init-packages/init-mu4e.el
+++ emacs/init-packages/init-mu4e.el
@@ -1,1 +1,7 @@
 (require 'mu4e)
+
+(setq mu4e-org-contacts-file  "~/doc/work/contacts.org")
+(add-to-list 'mu4e-headers-actions
+             '("org-contact-add" . mu4e-action-add-org-contact) t)
+(add-to-list 'mu4e-view-actions
+             '("org-contact-add" . mu4e-action-add-org-contact) t)
