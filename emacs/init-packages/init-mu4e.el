426cef0fe3380dac2daba78b203a6904119a2092
Damien Garaud <damien.garaud@gmail.com>
2017-07-03 08:59:31 +0200

    ---

--- emacs/init-packages/init-mu4e.el
+++ emacs/init-packages/init-mu4e.el
@@ -5,4 +5,5 @@
              '("org-contact-add" . mu4e-action-add-org-contact) t)
 (add-to-list 'mu4e-view-actions
              '("org-contact-add" . mu4e-action-add-org-contact) t)
+;; You have to install 'html2text' command
 (setq mu4e-html2text-command "html2text -utf8 -width 72")
