f4695c766aad897ffe301eb4732663d29a12fd2d
Damien Garaud <damien.garaud@gmail.com>
2018-02-25 14:54:10 +0100

    [dotfiles] update the cider REPL version

--- dotfiles/profiles.clj
+++ dotfiles/profiles.clj
@@ -1,5 +1,5 @@
 ;; Lein profiles
 {:user {:plugins [[lein-pprint "1.1.2"]
                   [luminus/lein-template "2.9.10.86"]
-                  [cider/cider-nrepl "0.15.1"]
+                  [cider/cider-nrepl "0.17.0-SNAPSHOT"]
                   [lein-ancient "0.6.10"]]}}
