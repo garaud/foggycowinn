5593a5ac8b1eb0973f86e3e62cc0c6d98c89388d
Damien Garaud <damien.garaud@gmail.com>
2018-03-31 18:17:48 +0200

    [emacs] ---

--- emacs/init-packages/init-dashboard.el
+++ emacs/init-packages/init-dashboard.el
@@ -2,7 +2,7 @@
 (dashboard-setup-startup-hook)
 
 (setq dashboard-items '((recents  . 5)
-                        (projects . 5)
-			(bookmarks . 5)
-                        (agenda . 5)
-			(registers . 5)))
+			(projects . 5)
+ 			(bookmarks . 5)
+			(agenda . 5)
+ 			(registers . 5)))
