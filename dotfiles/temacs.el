c3f63c554d1cadf8beaa57ea212c2aab34ac6a30
Damien Garaud <damien.garaud@gmail.com>
2018-06-06 22:48:39 +0200

    [emacs] update the emacs config file used just for terminal

--- dotfiles/temacs.el
+++ dotfiles/temacs.el
@@ -13,7 +13,21 @@
 ;; ;; Personal Emacs configuration file ;;
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'package)
+(add-to-list
+ 'package-archives
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+   t)
+
 (setq custom-file "~/.emacs.terminal.d/custom.el")
+(load-file custom-file)
 
 ;; ;;;;;;;;;;;;;
 ;; ;; General ;;
@@ -194,15 +208,5 @@ Goes backward if ARG is negative; error if CHAR not found."
                          (point))))
 
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(frame-background-mode (quote dark)))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
+(require 'projectile)
+(require 'virtualenvwrapper)
