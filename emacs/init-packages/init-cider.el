5c55c377f2b0b1bd9ee12168ef98121fbc025223
Damien Garaud <damien.garaud@gmail.com>
2018-11-24 10:11:44 +0100

    [emacs] update the initialization of Cider

--- emacs/init-packages/init-cider.el
+++ emacs/init-packages/init-cider.el
@@ -2,3 +2,7 @@
 
 (setq cider-overlays-use-font-lock t)
 (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
+
+;; use company-mode completion
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
