c7400d951831fbffcfa74de1421be66f38ed1fa8
Damien Garaud <damien.garaud@gmail.com>
2019-04-13 09:34:53 +0200

    ---

--- dotfiles/temacs.sh
+++ dotfiles/temacs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export TERM="xterm-256color"
 EMACS_USER_DIR="~/.emacs.terminal.d"
 DISPLAY=
 emacs -q --eval "(progn (setq user-emacs-directory \"${EMACS_USER_DIR}\") (setq user-init-file \"${EMACS_USER_DIR}/init.el\"))" -l $EMACS_USER_DIR/init.el $*
