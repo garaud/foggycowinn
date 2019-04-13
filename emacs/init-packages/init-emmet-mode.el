5a9394c1c25751cdffbfe8e6e79068d564be996f
Damien Garaud <damien.garaud@gmail.com>
2019-04-13 09:38:26 +0200

    [emacs] update the emmet-mode package configuration

--- emacs/init-packages/init-emmet-mode.el
+++ emacs/init-packages/init-emmet-mode.el
@@ -1,7 +1,8 @@
 (require 'emmet-mode)
 
+(setq emmet-move-cursor-between-quotes t)
+(setq emmet-indentation 2)
+(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
 (add-hook 'html-mode-hook 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook 'emmet-mode)
-(setq emmet-move-cursor-between-quotes t)
-(setq emmet-indentation 2)
