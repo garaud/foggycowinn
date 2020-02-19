d9d14f7b0bc47b759114bc7834e31f4f8ec710b4
Damien Garaud <damien.garaud@gmail.com>
2020-02-19 11:21:44 +0100

    [dotfiles] turn-off lispy for now

--- emacs/init-packages/init-lispy.el
+++ emacs/init-packages/init-lispy.el
@@ -1,4 +1,4 @@
-(require 'lispy)
+;; (require 'lispy)
 
-(add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
-(add-hook 'lisp-mode-hook (lambda () (lispy-mode 1)))
+;; (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+;; (add-hook 'lisp-mode-hook (lambda () (lispy-mode 1)))
