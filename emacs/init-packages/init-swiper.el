ee9e92e6316a4855ddeaa00741b814c19b5396e2
Damien Garaud <damien.garaud@gmail.com>
2020-02-19 11:36:13 +0100

    [emacs] update the swiper conf

--- emacs/init-packages/init-swiper.el
+++ emacs/init-packages/init-swiper.el
@@ -1,3 +1,4 @@
+
 (require 'swiper)
 (require 'counsel)
 (ivy-mode 1)
@@ -16,7 +17,7 @@
         (woman . "^")))
 
 (global-set-key "\C-s" 'swiper)
-;; (global-set-key "\C-x\b" 'ivy-switch-buffer)
+(global-set-key "\C-x\b" 'ivy-switch-buffer)
 
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
@@ -27,4 +28,4 @@
 ;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
 (global-set-key (kbd "C-c g") 'counsel-git)
 ;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
-;; (global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-c k") 'counsel-ag)
