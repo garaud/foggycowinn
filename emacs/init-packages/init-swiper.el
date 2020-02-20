e0664b7356bf518f22ea2167af4c0798b78bc14c
Damien Garaud <damien.garaud@gmail.com>
2020-02-20 21:43:19 +0100

    [emacs] update the swiper conf

--- emacs/init-packages/init-swiper.el
+++ emacs/init-packages/init-swiper.el
@@ -26,6 +26,6 @@
 ;; (global-set-key (kbd "<f1> l") 'counsel-find-library)
 ;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
 ;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
 ;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c f") 'counsel-git) ;; find file in the git repo
 (global-set-key (kbd "C-c k") 'counsel-ag)
