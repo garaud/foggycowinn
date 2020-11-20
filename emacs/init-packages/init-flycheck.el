91d36c4f29b0a3c8e6e416432a62115547096d32
Damien Garaud <damien.garaud@gmail.com>
2020-11-20 09:57:04 +0100

    [emacs] improve flycheck configuration

--- emacs/init-packages/init-flycheck.el
+++ emacs/init-packages/init-flycheck.el
@@ -1,2 +1,13 @@
 (require 'flycheck)
+
 (global-flycheck-mode)
+
+;; Tune the error list display
+;; https://www.flycheck.org/en/latest/user/error-list.html#tune-error-list-display
+(add-to-list 'display-buffer-alist
+             `(,(rx bos "*Flycheck errors*" eos)
+              (display-buffer-reuse-window
+               display-buffer-in-side-window)
+              (side            . bottom)
+              (reusable-frames . visible)
+              (window-height   . 0.30)))
