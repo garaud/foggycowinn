adfc5c4f9053b878e578bab0bff8407e3ec98911
Damien Garaud <damien.garaud@gmail.com>
2020-11-21 11:35:14 +0100

    [emacs] hl todo keywords faces

--- emacs/init-packages/init-hl-todo.el
+++ emacs/init-packages/init-hl-todo.el
@@ -3,3 +3,24 @@
 ;; highlight TODO/FIXME,etc. words
 
 (global-hl-todo-mode)
+
+
+(setq hl-todo-keyword-faces
+      '(("HOLD" . "#d0bf8f")
+        ("TODO" . "firebrick")
+        ("NEXT" . "#dca3a3")
+        ("THEM" . "#dc8cc3")
+        ("PROG" . "#7cb8bb")
+        ("OKAY" . "#7cb8bb")
+        ("DONT" . "#5f7f5f")
+        ("FAIL" . "#8c5353")
+        ("DONE" . "#8cfa8c")
+        ("NOTE" . "#d0bf8f")
+        ("KLUDGE" . "#d0bf8f")
+        ("HACK" . "#d0bf8f")
+        ("TEMP" . "#d0bf8f")
+        ("FIXME" . "#cc9393")
+        ("QUESTION" . "#7cb8bb")
+        ("XXX" . "red")
+        ("XXXX" . "#cc9393")
+        ("???" . "#cc9393")))
