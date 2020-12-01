9692c0d9fbcd5edc6b606b06b2748abe884c133a
Damien Garaud <damien.garaud@gmail.com>
2020-12-01 09:11:19 +0100

    [emacs] update the dashboard mode config

--- emacs/init-packages/init-dashboard.el
+++ emacs/init-packages/init-dashboard.el
@@ -1,8 +1,19 @@
 (require 'dashboard)
-(dashboard-setup-startup-hook)
+(require 'dashboard-widgets)
 
-(setq dashboard-items '((recents  . 5)
- 			(projects . 5)
-  			(bookmarks . 5)
+(setq dashboard-items '((recents  . 8)
+ 			(projects . 8)
  			(agenda . 5)
+  			(bookmarks . 5)
  			(registers . 5)))
+
+(setq dashboard-week-agenda t)
+(setq dashboard-startup-banner 'logo)
+(setq dashboard-projects-backend 'projectile)
+(setq dashboard-set-navigator t)
+(setq dashboard-set-init-info t)
+(setq dashboard-center-content t)
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+
+(dashboard-setup-startup-hook)
