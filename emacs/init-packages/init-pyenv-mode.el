968b717f2ff73b7434e1b29f2bbfe39cfc85035d
Damien Garaud <damien.garaud@gmail.com>
2020-12-08 10:42:00 +0100

    [emacs] set my own keymap for pyenv minor mode

--- emacs/init-packages/init-pyenv-mode.el
+++ emacs/init-packages/init-pyenv-mode.el
@@ -1,3 +1,12 @@
 (require 'pyenv-mode)
 
+(defvar customized-pyenv-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-p") 'pyenv-mode-set)
+    (define-key map (kbd "C-c C-u") 'pyenv-mode-unset)
+    map)
+  "Keymap for pyenv-mode.")
+
+(setcdr (assoc 'pyenv-mode minor-mode-map-alist) customized-pyenv-mode-map)
+
 (add-hook 'python-mode-hook 'pyenv-mode)
