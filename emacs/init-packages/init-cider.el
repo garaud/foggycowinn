1780c2d06a63eb47ce185f3882aa49051c556073
Damien Garaud <damien.garaud@gmail.com>
2020-12-11 10:18:33 +0100

    [emacs] update the cider configuration

--- emacs/init-packages/init-cider.el
+++ emacs/init-packages/init-cider.el
@@ -1,8 +1,14 @@
 (require 'cider)
 
+(setq cider-auto-mode t)
 (setq cider-overlays-use-font-lock t)
-(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
+
+;; it's quite strange but cider-mode is not enable by default when I open an .clj file
+(add-hook 'clojure-mode-hook #'cider-mode)
 
 ;; use company-mode completion
 (add-hook 'cider-repl-mode-hook #'company-mode)
 (add-hook 'cider-mode-hook #'company-mode)
+;; also use eldoc
+(add-hook 'cider-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
