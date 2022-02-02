39c23bcec63354c827043b6b7b9437344ec39c20
Damien Garaud <damien.garaud@gmail.com>
2022-02-02 13:52:18 +0100

    [emacs] update the lsp-mode configuration
    take into account scala and java langs

--- emacs/init-packages/init-lsp-mode.el
+++ emacs/init-packages/init-lsp-mode.el
@@ -3,6 +3,8 @@
 
 (setq lsp-enable-links nil)
 
+ (setq lsp-completion-provider :capf)
+
 ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
 (setq gc-cons-threshold (* 100 1024 1024)
       read-process-output-max (* 1024 1024)
@@ -11,6 +13,12 @@
       company-minimum-prefix-length 3)
 
 (with-eval-after-load 'lsp-mode
-  (require 'lsp-pyls)
-  (add-to-list 'lsp-enabled-clients 'pyls)
-  (add-hook 'python-mode-hook #'lsp))
+  (require 'lsp-pylsp)
+  (require 'lsp-java)
+  (add-to-list 'lsp-enabled-clients 'pylsp)
+  (add-to-list 'lsp-enabled-clients 'metals)
+  (add-to-list 'lsp-enabled-clients 'java)
+  (add-hook 'python-mode-hook #'lsp)
+  (add-hook 'scala-mode-hook #'lsp)
+  (add-hook 'java-mode-hook #'lsp)
+  (add-hook 'lsp-mode-hook #'lsp-lens-mode))
