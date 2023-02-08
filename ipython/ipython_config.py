954b2e4b075079a7e9fec4a2b00d53a89c210678
Damien Garaud <damien.garaud@gmail.com>
2023-02-08 09:39:05 +0100

    ipython: configure autoreload

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -5,7 +5,6 @@ from IPython.lib import deepreload
 # https://ipython.readthedocs.io/en/stable/api/generated/IPython.lib.deepreload.html
 
 # Configuration file for ipython.
-
 c = get_config()
 
 # Set the color scheme (NoColor, Linux, or LightBG).
@@ -18,7 +17,11 @@ c = get_config()
 # reload() function does not.  When deep_reload is off, IPython will use the
 # normal reload(), but deep_reload will still be available as dreload().
 builtins.reload = deepreload.reload
-# c.TerminalInteractiveShell.deep_reload = True
+
+# add the autorelaod extenstion by defaut.
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
 
 # Set to confirm when you try to exit IPython with an EOF (Control-D in Unix,
 # Control-Z/Enter in Windows). By typing 'exit' or 'quit', you can force a
